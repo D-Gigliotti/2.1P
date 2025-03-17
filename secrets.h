a43c2d2b091567ff1b99a314dbc74453be4b3003
D-Gigliotti <s224926842@deakin.edu.au>
2025-03-17 22:58:21 +1100

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,4 @@
+#define SECRET_SSID "Enter your network name"
+#define SECRET_PASS "Enter your password"
+#define SECRET_CH_ID Enter your Channel ID 
+#define SECRET_WRITE_APIKEY "Enter your API key"
